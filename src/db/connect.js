701717d44c3967f7c7c1e2df56fbc4abc2eaddc3
mucacran <el.mucacran.rasta@hotmail.com>
2023-01-16 05:18:31 -0500

    Estos archivos funcionan para conectarse a moongo

--- src/db/connect.js
+++ src/db/connect.js
@@ -4,32 +4,34 @@ const MongoClient = require('mongodb').MongoClient;
 
 let _db;
 
-const initDb = (callback) =>{
-    if(_db){
-        console.log('la base de datos esta inicializada');
-        return callback(null, _db);
-    }
-    MongoClient.conect(process.env.MONGODB_URI)
-    .then((client) =>{
-        _db = client;
-        callback(null, _db);
+const initDb = (callback) => {
+  if (_db) {
+    console.log('Db is already initialized!');
+    return callback(null, _db);
+  }
+  MongoClient.connect(process.env.MONGODB_URI)
+    .then((client) => {
+      _db = client;
+      callback(null, _db);
     })
-    .catch((err)=>{
-        callback(err);
+    .catch((err) => {
+      callback(err);
     });
 };
 
 const getDb = () => {
-    if(!_db){
-        throw Error('Db not se inicializo');
-    }
-    return _db;
+  if (!_db) {
+    throw Error('Db not initialized');
+  }
+  return _db;
 };
 
 module.exports = {
-    initDb,
-    getDb
+  initDb,
+  getDb,
 };
+
+
 
 
 
